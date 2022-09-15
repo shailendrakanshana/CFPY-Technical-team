38b91f87ca17db11cd37c1e7402ed27b625fefbd
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-15 10:36:46 +0530

    Time: 161 ms (69.38%), Space: 68.8 MB (26.90%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -4,8 +4,9 @@ class Solution {
             return 0;
         }
         boolean ar[] = new boolean[n];
-        for(int i=3; i<Math.sqrt(n); i=i+2){
+        for(int i=3; i<Math.sqrt(n); i=i+2){//i=i+2 because even can't be prime after 2
             if(ar[i]==false){
+                //marking all the multiples of i as true
                 for(int j=i*i; j<n; j= j +i){
                     ar[j] = true;
                 }

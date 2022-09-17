af801b152d9accbafe7a0ecd6314636789bfcf95
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-18 01:28:14 +0530

    Time: 1 ms (98.97%), Space: 42.1 MB (78.68%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -3,7 +3,7 @@ public class Solution {
     public int reverseBits(int n) {
         int r = 0;
         for(int i = 0;i<32;i++){
-            r = (r<<1) + ((n>>i)&1);
+            r = (r<<1) + ((n>>i)&1); // getting the last bit and setting it to right place
         }
         return r;
     }

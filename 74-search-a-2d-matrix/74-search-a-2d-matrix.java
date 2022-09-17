006d397c71ab4c48b0dea9dff889a26421e35e4a
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-18 00:00:35 +0530

    Time: 1 ms (39.14%), Space: 43.3 MB (8.08%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,19 +1,53 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int c = matrix[0].length-1;
-        int r = 0;
-        while(c>=0&&r<matrix.length){
-            if(matrix[r][c]==target){
+    //Efficient solution ever
+    
+    public static boolean binsearch(int ar[],int t){
+        int l = 0;
+        int h = ar.length-1;
+        int m = l - ((l-h)/2);
+        while(h>=l){
+            if(ar[m]==t){
                 return true;
             }
-            else if(matrix[r][c]>target){
-                c--;
+            if(ar[m]>t){
+                h = m-1;
             }
-            else if(matrix[r][c]<target){
-                r++;
+            if(ar[m]<t){
+                l = m+1;
             }
+            m = l - ((l-h)/2);
+            
         }
         return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int p = -1;
+        int l = 0;
+        int h = n-1;
+        int mid = l - ((l-h)/2);
+        while(h>=l){
+            if(matrix[mid][0]==target || matrix[mid][m-1]==target){
+                return true;
+            }
+            if(matrix[mid][0]<target && matrix[mid][m-1]>target){
+                p = mid;
+                break;
+            }
+            if(matrix[mid][0]>target){
+                h = mid-1;
+            }
+            if(matrix[mid][m-1]<target){
+                l = mid+1;
+            }
+            mid = l - ((l-h)/2);
+            
+        }
+        if(p==-1){
+            return false;
+        }
+        return binsearch(matrix[p],target);
         
     }
 }

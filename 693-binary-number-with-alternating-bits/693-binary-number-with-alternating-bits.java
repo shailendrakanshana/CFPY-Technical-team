90fdccc8fcb699cf20b85c9752db58d207b23f71
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-20 20:16:45 +0530

    Time: 1 ms (65.29%), Space: 41 MB (50.58%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.java
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean hasAlternatingBits(int n) {
         for(int i = 0;i<32;i++){
-            if((((n>>i)&1)==1)&&(((n>>i+1)&1)==1)){//checking two adjacent bits
+            if((((n>>i)&1)==1)&&(((n>>i+1)&1)==1)){//checking two adjacent bits for 1
                 return false;
             }
-            if((((n>>i)&1)==0)&&(((n>>i+1)&1)==0)){//checking two adjacent bits
+            if((((n>>i)&1)==0)&&(((n>>i+1)&1)==0)){//checking two adjacent bits for 0
                 return false;
             }
-            if((n>>i+1)==0){
+            if((n>>i+1)==0){//checking the bits completion
                 break;
             }
         }

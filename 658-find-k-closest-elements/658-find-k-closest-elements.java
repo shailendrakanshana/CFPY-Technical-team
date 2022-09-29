cb9013db36d275bc8fd071dcf33e3c015232a4d1
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-29 15:33:38 +0530

    Time: 36 ms (27.29%), Space: 63.1 MB (27.99%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> l = new ArrayList<>();
+        int n = arr.length;
+        if(x<=arr[0]){
+            for(int i = 0;i<k;i++){
+                l.add(arr[i]);
+            }
+            return l;
+        }
+        else if(x>=arr[n-1]){
+            int p = n-1;
+            while(k-->0){
+                l.add(arr[p]);
+                p--;
+            }
+            Collections.sort(l);
+            return l;
+        }
+        else{
+            int p = 0;
+            for(int i = 0;i<n-1;i++){
+                if(arr[i]<=x && arr[i+1]>=x){
+                    p = i;
+                    break;
+                }
+            }
+            int a = p;
+            int b = p+1;
+            while(a>=0 || b<n){
+                if(l.size()==k){
+                    break;
+                }
+                if(a<0){
+                    l.add(arr[b]);
+                    b++;
+                }
+                else if(b>n-1){
+                    l.add(arr[a]);
+                    a--;
+                }
+                else{
+                    int c = Math.abs(arr[a] -x);
+                    int d = Math.abs(arr[b]-x);
+                    if(c>d){
+                        l.add(arr[b]);
+                        b++;
+                    }
+                    else{
+                        l.add(arr[a]);
+                        a--;
+                    }
+                }
+            }
+            Collections.sort(l);
+           // System.out.println("Heelo");
+            return l;
+        }
+    }
+}

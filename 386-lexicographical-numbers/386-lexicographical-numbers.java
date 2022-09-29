fdf5d3482af396d2b925c763b41613f21056a854
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-29 14:47:09 +0530

    Time: 2 ms (100.00%), Space: 46.8 MB (92.33%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -1,5 +1,26 @@
 class Solution {
+    public static void lexSort(int curr,int n,List<Integer> l){
+        if(curr>n){
+            return;
+        }
+        else{
+            l.add(curr);
+            for(int i=0;i<10;++i){
+                if(10*curr+i>n)
+                    return;
+                lexSort(10*curr+i, n,l);
+            }
+        }
+    }
     public List<Integer> lexicalOrder(int n) {
+        List<Integer> l = new ArrayList<>();
+        for(int i = 1;i<10;i++){
+            lexSort(i,n,l);
+        }
+        return l;
+        
+        /*
+        //CHEATING methond
         List<String> l = new ArrayList<>();
         for(int i = 1;i<=n;i++){
             l.add(Integer.toString(i));
@@ -9,6 +30,6 @@ class Solution {
         for(int i = 0;i<n;i++){
             r.add(Integer.parseInt(l.get(i)));
         }
-        return r;
+        return r;*/
     }
 }

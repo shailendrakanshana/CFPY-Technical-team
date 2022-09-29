1bc1741efca9f383994906c0a5cec601dceeaa6e
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2022-09-29 14:08:44 +0530

    Time: 1 ms (97.83%), Space: 41.9 MB (99.71%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,12 +1,12 @@
 class Solution {
     public static void generate(int n,int open,int close,String ans,List<String> l){
-        if(open ==n && close==n){
+        if(open ==n && close==n){//For output
             l.add(ans);
         }
-        if(open<n){
+        if(open<n){//for adding opening parenthesis like (( and go on till open <n
             generate(n,open+1,close,ans+"(",l);
         }
-        if(open>close){
+        if(open>close){//for adding closing parethesis like (() and go on
             generate(n ,open,close+1,ans+")",l);
         }
     }

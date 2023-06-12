da3c0501fca7fe0bef291326c93f495bb8fe109c
shailendra singh kanshana <97683040+shailendrakanshana@users.noreply.github.com>
2023-06-12 17:36:10 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        // System.out.println(adj);
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> q = new Stack<>();
+        q.push(0);
+        HashSet<Integer> visited = new HashSet<>();
+        while(!q.isEmpty()){
+            int v = q.pop();
+            if(visited.contains(v)) continue;
+            visited.add(v);
+            ans.add(v);
+            ArrayList<Integer> l = adj.get(v);
+            for(int i = l.size()-1;i>=0;i--){
+                if(!visited.contains(l.get(i))) q.push(l.get(i));
+            }
+        }
+        
+        return ans;
+    }
+}
